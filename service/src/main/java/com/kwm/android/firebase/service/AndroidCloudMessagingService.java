2c3e458696f6c2a0db1ebe51263d4d358ba92343
King <kingwaimark@gmail.com>
2020-02-13 21:08:33 -0500

    Added new notificaiton features. Auto notifications

--- service/src/main/java/com/kwm/android/firebase/service/AndroidCloudMessagingService.java
+++ service/src/main/java/com/kwm/android/firebase/service/AndroidCloudMessagingService.java
@@ -1,14 +1,22 @@
 package com.kwm.android.firebase.service;
 
+
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
 
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class AndroidCloudMessagingService extends FirebaseMessagingService {
+    private boolean autoNotification = false;
+    private OnMessageReceived onMessageReceived;
+
     public AndroidCloudMessagingService(){
         super();
     }
@@ -34,9 +42,43 @@ public class AndroidCloudMessagingService extends FirebaseMessagingService {
         );
     }
 
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        if(this.autoNotification) {
+            try{
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(this, remoteMessage.getNotification().getChannelId())
+                        .setContentTitle(remoteMessage.getNotification().getTitle())
+                        .setContentText(remoteMessage.getNotification().getBody())
+                        .setStyle(new NotificationCompat.BigTextStyle()
+                                .bigText(remoteMessage.getNotification().getBody()))
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+                notificationManager.notify(Integer.parseInt(remoteMessage.getMessageId()), builder.build());
+            } catch (Exception err) {}
+        }
+
+        this.onMessageReceived.onReceived(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+    }
+    public void setOnMessageReceived(OnMessageReceived onMessageReceived) {
+        this.onMessageReceived = onMessageReceived;
+    }
+
+    public void autoNotification(){
+        this.autoNotification = true;
+    }
+    public void stopAutoNotification(){
+        this.autoNotification = false;
+    }
+
 
     public interface SubscribeListener {
         void onSuccess();
         void onFail();
     }
+    public interface OnMessageReceived {
+        void onReceived(String title, String body);
+    }
+
 }
