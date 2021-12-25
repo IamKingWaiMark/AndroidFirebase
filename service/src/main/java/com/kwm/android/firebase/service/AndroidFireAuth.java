469b82a0bbedbf02ef06ea783afba6e1124e80eb
KingWaiMark <kingwaimark@gmail.com>
2021-12-24 21:36:48 -0500

    Auth update bug fix 2

--- service/src/main/java/com/kwm/android/firebase/service/AndroidFireAuth.java
+++ service/src/main/java/com/kwm/android/firebase/service/AndroidFireAuth.java
@@ -6,6 +6,7 @@ import android.util.Log;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
