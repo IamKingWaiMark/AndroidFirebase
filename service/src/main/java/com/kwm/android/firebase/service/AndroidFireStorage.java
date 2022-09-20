bed8f72c4f7d986e10709083f4ff330d16bc5af7
KingWaiMark <kingwaimark@gmail.com>
2022-09-19 20:11:15 -0400

    Storage

--- service/src/main/java/com/kwm/android/firebase/service/AndroidFireStorage.java
+++ service/src/main/java/com/kwm/android/firebase/service/AndroidFireStorage.java
@@ -1,4 +1,25 @@
 package com.kwm.android.firebase.service;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 public class AndroidFireStorage {
+
+    private StorageReference getReference(String path) {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        if(path == null) {
+            return storage.getReference();
+        } else {
+            if(path.startsWith("gs:")) {
+                return storage.getReferenceFromUrl(path);
+            } else if(path.startsWith("http") || path.startsWith("https")) {
+                return storage.getReferenceFromUrl(path);
+            } else {
+                StorageReference storageRef = storage.getReference();
+                return storageRef.child(path);
+            }
+
+        }
+    }
+
 }
